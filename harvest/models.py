3b1e44175327f6efd13b089fa430e9a927c1d87b
Damian Kijo <damian.kijo@gmail.com>
2022-11-09 22:25:10 +0100

    added coffeorigincountry model

--- harvest/models.py
+++ harvest/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class CoffeeOriginCountry(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField('starting date of harvest')
+    end_date = models.DateField('end date of harvest')
