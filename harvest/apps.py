d5caa275d9806ab24d785dc56a8343d0e47dd55a
Damian Kijo <damian.kijo@gmail.com>
2022-11-09 21:55:22 +0100

    app renamed to harvest

--- harvest/apps.py
+++ harvest/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class StartSiteConfig(AppConfig):
+class HarvestConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'start_site'
+    name = 'harvest'
