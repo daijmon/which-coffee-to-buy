aa33c52200752f49b38c6ea04b71367ccf6a0044
Damian Kijo <damian.kijo@gmail.com>
2022-11-12 20:58:50 +0100

    changed to templateview

--- harvest/views.py
+++ harvest/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from .models import CoffeeOriginCountry
 
 
-def index(request):
-    all_countries = CoffeeOriginCountry.objects.all()
-    context = {'all_countries': all_countries}
-    return render(request, 'harvest/index.html', context)
+class CoffeeFromCountryView(TemplateView):
+    template_name = "harvest/coffee-from-country.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        all_countries = CoffeeOriginCountry.objects.all()
+        context['all_countries'] = all_countries
+        return context
