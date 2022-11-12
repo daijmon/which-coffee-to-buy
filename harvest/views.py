57430489e36dbb58fcf60ee2a36726a988efec52
Damian Kijo <damian.kijo@gmail.com>
2022-11-12 22:31:39 +0100

    added filter for valid coutries to buy coffee from

--- harvest/views.py
+++ harvest/views.py
@@ -7,13 +7,18 @@ from .models import CoffeeOriginCountry
 
 class CoffeeFromCountryView(TemplateView):
     template_name = "harvest/coffee-from-country.html"
+    COFFEE_TRANSPORT_DELAY = 2  # months
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        all_countries = CoffeeOriginCountry.objects.all()
-        context['all_countries'] = all_countries
-        context['today'] = self._current_month()
+        context['valid_countries'] = self.which_coffee_to_buy()
         return context
 
     def _current_month(self):
         return datetime.today().month
+
+    def which_coffee_to_buy(self):
+        harvest_month = self._current_month() - self.COFFEE_TRANSPORT_DELAY
+        valid_countries = CoffeeOriginCountry.objects.filter(start_date__month__gte=harvest_month,
+                                                             end_date__month__lte=harvest_month)
+        return valid_countries
