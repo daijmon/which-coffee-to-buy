027f39c4b525bdbe6486d36dd2f5189c83b0444a
Damian Kijo <damian.kijo@gmail.com>
2022-11-12 22:09:30 +0100

    added current_month method

--- harvest/views.py
+++ harvest/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.views.generic import TemplateView
 
 from .models import CoffeeOriginCountry
@@ -10,4 +12,8 @@ class CoffeeFromCountryView(TemplateView):
         context = super().get_context_data(**kwargs)
         all_countries = CoffeeOriginCountry.objects.all()
         context['all_countries'] = all_countries
+        context['today'] = self._current_month()
         return context
+
+    def _current_month(self):
+        return datetime.today().month
