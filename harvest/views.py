f46bf1d42429cbda958ee8bff6a91b83f2ae0329
Damian Kijo <damian.kijo@gmail.com>
2022-11-13 10:43:25 +0100

    which coffe to buy logic drafted

--- harvest/views.py
+++ harvest/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from django.db.models import F
 from django.views.generic import TemplateView
 
 from .models import CoffeeOriginCountry
@@ -7,7 +8,7 @@ from .models import CoffeeOriginCountry
 
 class CoffeeFromCountryView(TemplateView):
     template_name = "harvest/coffee-from-country.html"
-    COFFEE_TRANSPORT_DELAY = 2  # months
+    COFFEE_TRANSPORT_DELAY = 3  # months
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -23,8 +24,11 @@ class CoffeeFromCountryView(TemplateView):
         return datetime.today().month
 
     def which_coffee_to_buy(self) -> list[CoffeeOriginCountry]:
-        valid_countries = CoffeeOriginCountry.objects.filter(
-            start_date__month__gte=self.harvest_month, end_date__month__lte=self.harvest_month
+        all_countries_annotated = CoffeeOriginCountry.objects.all().annotate(
+            middle_of_harvest=((F("start_date__month") + F("end_date__month")) / 2))
+        valid_countries = all_countries_annotated.filter(
+            middle_of_harvest__lte=self.harvest_month,
+            end_date__month__gte=self.harvest_month,
         )
         return self._sort_countries_based_on_harvest(valid_countries)
 
